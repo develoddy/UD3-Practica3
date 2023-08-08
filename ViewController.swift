ea396fdf9ec6f48aad837645e736871c6bf76e1a
Eddy Lujan <eddylujann@gmail.com>
2023-08-08 17:48:10 +0200

    Práctica 3 - Autolayout
    Práctica 3 - Autolayout

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UD3_Practica3
+//
+//  Created by Eddy Donald Chinchay Lujan on 8/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - Properties
+    private let deepskyblueColor = UIColor(red: 0.00, green: 0.75, blue: 1.00, alpha: 1.00)
+    private let dimgrayColor = UIColor(red: 0.41, green: 0.41, blue: 0.41, alpha: 1.00)
+    private let blackColor = UIColor(red: 0.00, green: 0.00, blue: 0.00, alpha: 1.00)
+    private let lightYellowColor = UIColor(red: 0.94, green: 0.90, blue: 0.55, alpha: 1.00)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
